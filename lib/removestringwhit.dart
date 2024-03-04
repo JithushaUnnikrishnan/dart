71c6745627eea519d29a1f215f0c3e7ac429293a
JithushaUnnikrishnan <161579053+JithushaUnnikrishnan@users.noreply.github.com>
2024-03-04 23:22:07 +0530

    first commit

---
+++ lib/removestringwhit.dart
@@ -0,0 +1,7 @@
+void main()
+{
+  String sentence='iam jithusha iam from malappuram' ;
+  String sentencewithoutspace=sentence.replaceAll(' ', '');
+  print("sentence with space=$sentence \nsentence without space=$sentencewithoutspace");
+
+}
---
+++ lib/swapping.dart
@@ -0,0 +1,11 @@
+void main()
+{
+  int a=5;
+  int b=23;
+  print("before swapping: \n a=$a \n b=$b");
+
+  int temp=a;
+  a=b;
+  b=temp;
+  print('after swapping: \n a=$a \n b=$b');
+}
